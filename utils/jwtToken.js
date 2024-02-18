b1984bef4a802c50122aa6d59201785a735c58bb
nbtmik <127678334+nbtmik@users.noreply.github.com>
2024-02-18 21:09:23 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,9 +6,9 @@ const sendToken = (user,statusCode,res)=>{
         expires:new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // calucate in milisec
         ),
-        secure: true,
+        secure: true,//use this when the code is in production for https cookie request
         httpOnly:true,
-        sameSite: 'None',
+        sameSite: 'None',//dealing with cross-site requests and the usage of third-party cookies
     };
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
